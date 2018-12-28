5345df881022114b75f344367b2c3574df19f6ab
Sarah Paz <sarah.paz@gmail.com>
2018-12-28 06:42:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(katz_deli)
   else
     current_line = "The line is currently:"
     katz_deli.each_with_index { |name, index|
-    current_line << " #{index}. #{name}" }
-  end
+    current_line << " #{index + 1}. #{name}" }
     puts current_line
+  end
 end
 
 def take_a_number(katz_deli, new_customer)
